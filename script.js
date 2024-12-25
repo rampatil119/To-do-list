64cd09a9d9a8a69aca0aeb40650b55ada42ef07a
rampatil119 <rameshwarpatil9689@gmail.com>
2024-12-25 16:22:55 +0530

    create todo list

---
+++ script.js
@@ -0,0 +1,17 @@
+const inputBox=document.getElementById('inputBox');
+const addBtn=document.getElementById('addBtn');
+const todoList=document.getElementById('todoList');
+const sub=document.getElementById('sub')
+
+
+
+const add=()=>{
+    alert("hello ram")
+}
+
+sub.addEventListener('click',add)
+
+addBtn.addEventListener('click',addTodo)
+
+
+
